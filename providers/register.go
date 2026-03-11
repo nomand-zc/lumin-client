0cb36036416f7003efab00bfb6342c54d0adf7e1
berryczhang <berryczhang@tencent.com>
2026-03-11 16:44:32 +0800

    修改注释

--- providers/register.go
+++ providers/register.go
@@ -2,7 +2,7 @@ package providers
 
 var registeredProviders = make(map[string]map[string]Provider)
 
-// Register 注册一个 provider
+// Register 注册一个 provider到全局管理器，可以作为单例使用
 func Register(provider Provider) {
 	providerType, providerName := provider.Type(), provider.Name()
 	if registeredProviders[providerType] == nil {
